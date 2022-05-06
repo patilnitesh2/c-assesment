67dfa23c02c569975db006b59a8385e513116279
patilnitesh2 <102574050+patilnitesh2@users.noreply.github.com>
2022-05-06 08:00:46 +0000

    c# Execption

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using NiteshConsoleApp1;
+using System;
+using System.Collections.Generic;
+using static NiteshConsoleApp1.StackException;
+
+public class Exeption
+{
+	public static void Main()
+	{
+		Stack<int> numbers = new Stack<int>();
+		try
+		{
+			
+			numbers.Push(1);
+			numbers.Push(2);
+			numbers.Push(3);
+			numbers.Push(4);
+			numbers.Push(5);
+			numbers.Pop();
+
+		}
+		catch (StackException sfe)
+		{
+			Console.WriteLine(sfe.ToString());
+		}
+
+	}
+}
---
+++ StackException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NiteshConsoleApp1
+{
+
+    internal class StackException : ApplicationException
+    {
+        public StackException() { }
+
+        public StackException(string message)
+            : base(message) { }
+
+    }
+}
